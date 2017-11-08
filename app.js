33f1980bc04bde95d112a2b5ddf734d99961ed96
Salvatore Laisa <moebiusmania@gmail.com>
2017-11-08 17:31:15 +0100

    Updates

--- app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const gh = document.querySelector('github-user');
-const sf = document.querySelector('search-field');
-
-sf.addEventListener('search', evt => {
-  console.log(evt.detail);
-  gh.name = evt.detail.data;
-});
